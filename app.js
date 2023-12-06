07ca9da1bdc0f14e8a0ac0ba92242d1ef1e5740e
FJAP96x <lx02918@innovacion.mx>
2023-12-06 10:52:49 -0600

    segundo commit

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-let total=0;
-const correctas=["Roma","Oslo","Lima","Estocolmo","Ottawa"];
+let total = 0;
+const correctas = ["Roma", "Oslo", "Lima", "Estocolmo", "Ottawa"];
 
-const Aciertos=document.getElementById("Aciertos")
-const boton=document.getElementById("correctas")
+const Aciertos = document.getElementById("Aciertos");
+const boton = document.getElementById("correctas");
 
-boton.addEventListener("click",function(){
-    let opciones= document.querySelectorAll("input:checked");
-    let opcionsel=[...opciones].map(opcion => opcion.value)
-    for(let i=0;i<opciones.length;i++){
-        if(correctas[i] === opcionsel[i]){
-            total++;
-        }
+
+boton.addEventListener("click", function () {
+  let opciones = document.querySelectorAll("input:checked");
+  let opcionsel = [...opciones].map((opcion) => opcion.value);
+  for (let i = 0; i < opciones.length; i++) {
+    if (correctas[i] === opcionsel[i]) {
+    total++;
     }
-
-    Aciertos.innerHTML="Aciertos: "+ total;
-   
-    })
+  }
+  Aciertos.innerHTML = "Aciertos: " + total;
+});
